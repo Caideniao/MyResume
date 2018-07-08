4acf64d2b9ea6a2414342b9705ba6e7272a5b115
Caideniao <yzqtdu@gmail.com>
2018-07-09 00:06:07 +0800

    add resume content

--- main.js
+++ main.js
@@ -3,8 +3,20 @@ function shotEnlarge() {
     for (let i of shots) {
         i.addEventListener('click',enlarge)
     }
+    let s = document.querySelector('.cancelPop')
+    s.addEventListener('click',cancel)
+    
 }
 function enlarge(e) {
-    let d = querySelector('.enlarge')
-    let 
+    let imageURL = e.target.src
+    let d = document.querySelector('.enlarge')
+    let img = document.querySelector('.largeShot')
+    d.classList.add('pop-image')
+    img.src = imageURL
+} 
+function cancel(e) {
+    let d = document.querySelector('.enlarge')
+    d.classList.remove('pop-image')
+    
 }
+shotEnlarge()
