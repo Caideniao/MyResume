d6355011de12d6a24c149858a96adc18ce523f13
Caideniao <yzqtdu@gmail.com>
2018-07-08 18:01:11 +0800

    myresume

---
+++ main.js
@@ -0,0 +1,10 @@
+let shots = document.querySelectorAll('.shot')
+function shotEnlarge() {
+    for (let i of shots) {
+        i.addEventListener('click',enlarge)
+    }
+}
+function enlarge(e) {
+    let d = querySelector('.enlarge')
+    let 
+}
